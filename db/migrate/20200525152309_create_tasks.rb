2ee11425d9f14f6853803e70fa64939f3e3a793d
lauranne depatoul <lauranne.depatoul@gmail.com>
2020-05-25 17:43:01 +0200

    models push

---
+++ db/migrate/20200525152309_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.date :duedate
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
