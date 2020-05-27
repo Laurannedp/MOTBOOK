dcb1099b4f383567b2cf087676aeaf5d4337ed57
Pietro Fouarge <pietrofouarge@gmail.com>
2020-05-27 12:28:44 +0200

    gem emoticon added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require jquery
+require rails_emoji_picker
+require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,6 +26,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
