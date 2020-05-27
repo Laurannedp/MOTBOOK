4698cae29914ae0db33166a0912a7302697bd57e
lauranne depatoul <lauranne.depatoul@gmail.com>
2020-05-27 11:45:21 +0200

    routes avec action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: 'mots#index'
+  devise_for :users
   resources :mots do
     resources :tasks, only: [:new, :create, :edit, :update] do
       resources :actions, only: [:new, :create, :edit, :update]
     end
   end
   resources :tasks, only: [:index, :destroy]
-  resources :action, only: [:index, :destroy]
+  resources :actions, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
