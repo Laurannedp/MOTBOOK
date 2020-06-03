cd59d33763edd68dace849db56d6471b5f4c76dd
thibaut dehem <thibaut.dehem@gmail.com>
2020-06-03 16:54:29 +0200

    done

---
+++ app/javascript/packs/bootstrap_component.js
@@ -0,0 +1,10 @@
+const tooltip = () => {
+        $(function () {
+          const bulle = document.querySelector("#bulle")
+          // bulle.tooltip()
+          // $('#bulle').tooltip()
+            console.log(document.querySelector("#bulle")
+      })
+  }
+
+export { tooltip }
