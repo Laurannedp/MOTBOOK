7f822868c8317e38cb63e640241ff7650a612bc6
Pietro Fouarge <pietrofouarge@gmail.com>
2020-06-04 11:39:04 +0200

    whatsapp done

---
+++ app/services/twilio_service.rb
@@ -0,0 +1,25 @@
+require 'twilio-ruby'
+class TwilioService
+  def initialize(trigger)
+    account_sid = 'AC1e0e9c14499513e88e26c05b49b3b22e'
+    auth_token = token
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+
+    message = @client.messages.create(
+                                 body: "#{trigger.name} : #{trigger.url}",
+                                 from: 'whatsapp:+14155238886',
+                                 to: 'whatsapp:+32476859267'
+                               )
+
+    puts message.sid
+  end
+
+
+  private
+
+  def token
+   ENV['TWILIO_TOKEN']
+  end
+end
+
