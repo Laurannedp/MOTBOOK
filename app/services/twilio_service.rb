f78a8b9d95f28638336df9caedd424b66ed47847
Pietro Fouarge <pietrofouarge@gmail.com>
2020-06-04 16:31:36 +0200

    trigger table updated

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -5,11 +5,10 @@ class TwilioService
     auth_token = token
     @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-
     message = @client.messages.create(
                                  body: "#{trigger.name} : #{trigger.url}",
                                  from: 'whatsapp:+14155238886',
-                                 to: 'whatsapp:+32476859267'
+                                 to: 'whatsapp:+32479756915'
                                )
 
     puts message.sid
